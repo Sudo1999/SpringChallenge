a8cd97d71783834a60ffacad3fc4f2f9d016ee4c
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2024-04-01 15:16:30 +0200

    PixelRendering

--- src/main/java/com/coding/games/PixelRendering.java
+++ src/main/java/com/coding/games/PixelRendering.java
@@ -1,31 +1,71 @@
 package com.coding.games;
 
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 public class PixelRendering {
 
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        if (in.hasNextLine()) {
-            in.nextLine();
-        }
+        //Scanner in = new Scanner(System.in);
 
-        // game loop
-        while (true) {
-            String command = in.nextLine();
-            for (int i = 0; i < n; i++) {
+        try {
+            String inputFile = "src/main/resources/2-PixelRendering.txt";
+            Scanner in = new Scanner(new File(inputFile));
 
-                // Write an action using System.out.println()
-                // To debug: System.err.println("Debug messages...");
+            int n = in.nextInt();
+            if (in.hasNextLine()) {
+                in.nextLine();
+            }
+            List<String> image = new ArrayList<>();
+            for (int row = 0; row < n; row++) {
+                String string = "";
+                for (int col = 0; col < n; col++) {
+                    string += ".";
+                }
+                image.add(string);
+            }
+
+            while (in.hasNextLine()) {
+                /*  A chaque tour vous recevez une instruction représentée par deux valeurs : un type (C pour column ou R pour row),
+                *  et une coordonnée (le numéro de la colonne ou de la ligne concernée). Soit tous les pixels de la colonne deviennent noirs ("#"),
+                *  soit tous les pixels de la ligne deviennent blancs ("."). Vous devez afficher l'image après chaque commande reçue. */
+
+                String command = in.nextLine();
+                String type = command.split(" ")[0];
+
+                if (type.equals("=>")) {
+                    // Image finale attendue
+                    String expected = command.substring(3);
+                    System.out.println(image);
+                    System.out.println(image.toString().equals(expected));
+
+                } else {
+                    // Evolution de l'image
+                    int coord = Integer.valueOf(command.split(" ")[1]);
+
+                    if (type.equals("C")) {
+                        for (int row = 0; row < image.size(); row++) {
+                            String string = image.get(row);
+                            string = string.substring(0, coord) + "#" + string.substring(coord + 1);
+                            image.set(row, string);
+                        }
+                    } else if (type.equals("R")){
+                        String string = "";
+                        for (int col = 0; col < image.get(0).length(); col++) {
+                            string += ".";
+                        }
+                        image.set(coord, string);
+                    }
+                }
 
                 // Print the i-th line of the image after the command was executed
-                System.out.println(".#.#.");
+                //System.out.println(".#.#.");
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
