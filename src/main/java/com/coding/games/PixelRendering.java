b56365553c15dd714b7aa26b4e25ecca480ba301
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2024-04-01 17:21:18 +0200

    PixelRendering

--- src/main/java/com/coding/games/PixelRendering.java
+++ src/main/java/com/coding/games/PixelRendering.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
 
 public class PixelRendering {
 
+    /*  A chaque tour vous recevez une instruction représentée par deux valeurs : un type (C pour column ou R pour row),
+     *  et une coordonnée (le numéro de la colonne ou de la ligne concernée). Soit tous les pixels de la colonne deviennent noirs ("#"),
+     *  soit tous les pixels de la ligne deviennent blancs ("."). Vous devez afficher l'image après chaque commande reçue. */
+
     public static void main(String args[]) {
         //Scanner in = new Scanner(System.in);
 
@@ -16,54 +20,44 @@ public class PixelRendering {
             Scanner in = new Scanner(new File(inputFile));
 
             int n = in.nextInt();
+            String expected = "";
+            List<String> image = new ArrayList<>();
+            for (int row = 0; row < n; row++) {
+                image.add(".".repeat(n));
+            }
+
             if (in.hasNextLine()) {
                 in.nextLine();
             }
-            List<String> image = new ArrayList<>();
-            for (int row = 0; row < n; row++) {
-                String string = "";
-                for (int col = 0; col < n; col++) {
-                    string += ".";
-                }
-                image.add(string);
-            }
 
+            // game loop
             while (in.hasNextLine()) {
-                /*  A chaque tour vous recevez une instruction représentée par deux valeurs : un type (C pour column ou R pour row),
-                *  et une coordonnée (le numéro de la colonne ou de la ligne concernée). Soit tous les pixels de la colonne deviennent noirs ("#"),
-                *  soit tous les pixels de la ligne deviennent blancs ("."). Vous devez afficher l'image après chaque commande reçue. */
-
                 String command = in.nextLine();
                 String type = command.split(" ")[0];
+                int coord = Integer.parseInt(command.split(" ")[1]);
 
-                if (type.equals("=>")) {
-                    // Image finale attendue
-                    String expected = command.substring(3);
-                    System.out.println(image);
-                    System.out.println(image.toString().equals(expected));
-
-                } else {
-                    // Evolution de l'image
-                    int coord = Integer.valueOf(command.split(" ")[1]);
-
-                    if (type.equals("C")) {
-                        for (int row = 0; row < image.size(); row++) {
-                            String string = image.get(row);
-                            string = string.substring(0, coord) + "#" + string.substring(coord + 1);
-                            image.set(row, string);
-                        }
-                    } else if (type.equals("R")){
-                        String string = "";
-                        for (int col = 0; col < image.get(0).length(); col++) {
-                            string += ".";
-                        }
-                        image.set(coord, string);
+                // Evolution de l'image
+                if (type.equals("C")) {
+                    for (int row = 0; row < image.size(); row++) {
+                        String string = image.get(row);
+                        string = string.substring(0, coord) + "#" + string.substring(coord + 1);
+                        image.set(row, string);
                     }
+                } else if (type.equals("R")) {
+                    String string = ".".repeat(n);
+                    image.set(coord, string);
                 }
-
-                // Print the i-th line of the image after the command was executed
-                //System.out.println(".#.#.");
+                // Image finale attendue
+                else if (type.equals("=>")) {
+                    expected = command.substring(5);
+                }
             }
+
+            for (int i = 0; i < n; i++) {
+                System.out.println(image.get(i));
+            }
+            System.out.println(image.toString().equals(expected));
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
